4c835565348b55ee9a21af9fd1a9e943ce163a06
minsu-zip <alstn4814@gmail.com>
2020-09-23 01:05:22 +0900

    Updates

---
+++ precache-manifest.b2920b380c7beeb893a49f932b28ee2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe8ac5a2976d4f957bc7881a8a00c5e",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "476bc73132cc8da6ac37",
+    "url": "/twitter/static/css/main.e7af341c.chunk.css"
+  },
+  {
+    "revision": "32ccd05b31cd5f8181a4",
+    "url": "/twitter/static/js/2.146c098d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/twitter/static/js/2.146c098d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476bc73132cc8da6ac37",
+    "url": "/twitter/static/js/main.b4317de6.chunk.js"
+  },
+  {
+    "revision": "9f20f10883ff1630816b",
+    "url": "/twitter/static/js/runtime-main.39cb9049.js"
+  }
+]);
